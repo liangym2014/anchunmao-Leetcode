6ca377212ab400d083f9271551da6be66876b2f9
anchunmao <79773229+liangym2014@users.noreply.github.com>
2022-01-19 02:09:50 -0800

    Create 2139. Minimum Moves to Reach Target Score - medium.cpp

---
+++ 2139. Minimum Moves to Reach Target Score - medium.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/minimum-moves-to-reach-target-score/
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        if(target == 1)
+            return 0;
+        
+        if(target & 1)
+            return 1 + minMoves(target - 1, maxDoubles);
+        
+        if(maxDoubles)
+            return 1 + minMoves(target >> 1, maxDoubles - 1);
+
+        return target - 1;
+        
+    }
+};
