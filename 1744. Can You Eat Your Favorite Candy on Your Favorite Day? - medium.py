c81dab31a7224a766c20f316e92c536aa974aef9
anchunmao <79773229+liangym2014@users.noreply.github.com>
2022-03-31 17:35:28 -0700

    Update 1744. Can You Eat Your Favorite Candy on Your Favorite Day? - medium.py

--- 1744. Can You Eat Your Favorite Candy on Your Favorite Day? - medium.py
+++ 1744. Can You Eat Your Favorite Candy on Your Favorite Day? - medium.py
@@ -3,14 +3,14 @@ class Solution:
     def canEat(self, candiesCount: List[int], queries: List[List[int]]) -> List[bool]:
         # queries[i] = [favoriteTypei, favoriteDayi, dailyCapi]
         ans = [True] * len(queries)
-        prev = [0] * len(candiesCount)
-        for i in range(len(candiesCount) - 1):
-            prev[i + 1] = candiesCount[i] + prev[i]
+
+        for i in range(1,len(candiesCount)):
+            candiesCount[i] += candiesCount[i - 1]
         
         for i, q in enumerate(queries):
             candy, day, cap = q
-            mn = prev[candy]
-            mx = prev[candy] + candiesCount[candy]
+            mn = candiesCount[candy - 1] if candy else 0
+            mx = candiesCount[candy]
             # assume on the favorite day i we manage to eat the xth candy of type i, x must meet both requirements:
             # (1) mn < x < mx;   (2) day < x < (day + 1) * cap;
             # therefore, interval (mn, mx) and (day, (day + 1) * cap) must intersect
