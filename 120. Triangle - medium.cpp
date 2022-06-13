0cbd03408c83994727b27bd1030ee59ac9eebf44
anchunmao <79773229+liangym2014@users.noreply.github.com>
2022-06-13 01:36:32 -0700

    Create 120. Triangle - medium.cpp

---
+++ 120. Triangle - medium.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/triangle/
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        for(int i = 1; i < n; i ++){
+            triangle[i][0] += triangle[i - 1][0];
+            for(int j = 1; j < i; j ++){
+                triangle[i][j] += min(triangle[i - 1][j - 1], triangle[i - 1][j]);
+            }
+            triangle[i].back() += triangle[i - 1].back();
+        }
+        return *min_element(triangle[n-1].begin(), triangle[n-1].end());
+    }
+};
